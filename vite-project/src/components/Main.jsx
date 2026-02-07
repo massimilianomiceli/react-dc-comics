fc9ddcf85e1fad00a49de87378a8855275131dbe
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-02-07 16:54:20 +0100

    inserito pulsante load more

--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import ArticleSection from "./ArticleSection"
+import '../index.css'
 
 export default function Main(){
     return(
@@ -7,6 +8,9 @@ export default function Main(){
             <div className="container-fluid bg-black">
                 <div className="container">
                     <ArticleSection />
+                    <div className="row justify-content-center">
+                        <button class="btnLoad mb-3" type="button">LOAD MORE</button>
+                    </div>
                 </div>
                     
 
