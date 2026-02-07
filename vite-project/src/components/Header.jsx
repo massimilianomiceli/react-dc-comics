76912b8d6a3633d21714ccbd84ee0b21d6f44176
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-02-07 15:00:31 +0100

    aggiunta animazione al passaggio del mouse su navbar, valorizzato contenuto della navbar con array di oggetti, creato modulo css dedicato per navbar

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,26 +1,12 @@
+import Navbar from "./Navbar"
 import '../index.css'
 
 export default function Header(){
     return(
         <>
         <header>
-            <div className="container">
-                <nav className="d-flex justify-content-around align-items-center">
-                    <img src="/img/dc-logo.png" id="logo" alt="" srcset="" />
-                    <ul className="d-flex h-100 my-0 list-unstyled gap-3">
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">CHARACTERS</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">COMICS</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">MOVIES</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">TV</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">GAMES</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">COLLECTIBLES</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">VIDEOS</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">FANS</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">NEWS</a></li>
-                        <li className=""><a href="#" className="text-secondary fw-bold text-decoration-none">SHOP</a></li>
-                    </ul>
-                </nav> 
-            </div>
+                <Navbar />
+                <div id="jumbotron"></div>
         </header>
         </>
     )
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import styles from "./Navbar.module.css"
+import '../index.css'
+
+export default function Navbar(){
+    const links = [
+        {text: "characters", url:"#", active: false},
+        {text: "comics", url:"#", active: true},
+        {text: "movies", url:"#", active: false},
+        {text: "tv", url:"#", active: false},
+        {text: "games", url:"#", active: false},
+        {text: "collectibles", url:"#", active: false},
+        {text: "videos", url:"#", active: false},
+        {text: "fans", url:"#", active: false},
+        {text: "news", url:"#", active: false},
+        {text: "shop", url:"#", active: false},
+    ]
+    return(
+        <>
+        <nav className={`${styles.navbar} d-flex justify-content-around align-items-center`}>
+            <img src="/img/dc-logo.png" id="logo" alt="DC logo" srcset="" />
+            <ul className="d-flex align-items-stretch h-100 my-0 list-unstyled gap-4">
+                {links.map((link, index)=>(
+                    <li className="d-flex align-items-stretch">
+                        <a href={link.url} className={`${styles.link} ${link.active ? styles.linkActive : ""}  d-flex align-items-center fw-bold fs-6 text-decoration-none`}>
+                            {link.text.toUpperCase()}
+                        </a>
+                    </li>                
+                ))}
+            </ul>
+        </nav>        
+        </>
+    )
+}
