d4b0bf2b765292a32a24241b54dc9dcb5d1ec458
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-02-07 09:04:57 +0100

    suddivisione in componenti header main e footer, con prima bozza navbar

---
+++ vite-project/src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main(){
+    return(
+        <>
+        <main>
+            <div className="container-fluid">
+                <section className="row bg-black">
+                    <h1>Content goes here</h1>
+                </section>
+            </div>
+        </main>
+        </>
+    )
+}
